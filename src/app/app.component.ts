ccd5b197336bba9281a7d0d990c98611921583fc
tchainaf <thainafdamasio@gmail.com>
2020-05-24 22:07:11 -0300

    tela ok

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { SESSION_STORAGE, StorageService } from 'ngx-webstorage-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'poc-sessionstorage';
+  info: string = "";
+  show: boolean = false;
+  dadoSalvo: string = "";
+
+  constructor(@Inject(SESSION_STORAGE) private storage: StorageService) {
+  }
+
+  salvarDado(): void {
+
+  }
+
+  buscarDado(): void {
+
+  }
+
+  deletarDado(): void {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { StorageServiceModule} from 'ngx-webstorage-service';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
     StorageServiceModule
   ],
